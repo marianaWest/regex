736bc9a7bd8b10ca5198910526161246319591ac
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2023-05-16 09:35:16 +0100

    check with lookahead

--- checkPassword.js
+++ checkPassword.js
@@ -2,3 +2,7 @@
 let password = "abc123";
 let checkPass = /(?=\w{3,6})(?=\D*\d)/;
 checkPass.test(password);
+
+// Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/;
